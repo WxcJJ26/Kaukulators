8dfac700ab88cb738e48c64e1748d5bc6931fc5a
WxcJJ26 <jjnkproxd@gmail.com>
2022-04-12 09:32:59 +0300

    Update Calculator.py
    op kalkulators

--- Calculator.py
+++ Calculator.py
@@ -2,22 +2,41 @@ from tkinter import*
 manslogs=Tk()
 manslogs.title("Calculator")
 #manslogs.geometry("300x300")
-btn0=Button(manslogs,text="0",padx="40",pady="20")
-btn1=Button(manslogs,text="1",padx="40",pady="20")
-btn2=Button(manslogs,text="2",padx="40",pady="20")
-btn3=Button(manslogs,text="3",padx="40",pady="20")
-btn4=Button(manslogs,text="4",padx="40",pady="20")
-btn5=Button(manslogs,text="5",padx="40",pady="20")
-btn6=Button(manslogs,text="6",padx="40",pady="20")
-btn7=Button(manslogs,text="7",padx="40",pady="20")
-btn8=Button(manslogs,text="8",padx="40",pady="20")
-btn9=Button(manslogs,text="9",padx="40",pady="20")
 
-btnsum=Button(manslogs,text="+",padx="40",pady="20")
-btnsub=Button(manslogs,text="-",padx="40",pady="20")
-btnmul=Button(manslogs,text="x",padx="40",pady="20")
-btndiv=Button(manslogs,text=":",padx="40",pady="20")
-btnclear=Button(manslogs,text="Del",padx="40",pady="20")
+def btnclick(number):
+    current=e.get() #nolasa esošo skaitli
+    e.delete(0,END) #nodzēš
+    newnumber=str(current)+str(number)
+    e.insert(0,newnumber) #ievieto displejā
+    return 0
+
+def btncommand (command):
+    global num1 #jaiegaume skaitlis
+    global mathOp #matemātiskais operātors
+    mathOp = command # +,-,x,:
+    num1=int(e.get())
+    e.delete(0,END)
+    return 0
+
+e=Entry(manslogs,width=15,font=("Arial Black", 20))
+e.grid(row=0,column=0,columnspan=5)
+
+btn0=Button(manslogs,text="0",padx="40",pady="20",command=lambda:btnclick(0))
+btn1=Button(manslogs,text="1",padx="40",pady="20",command=lambda:btnclick(1))
+btn2=Button(manslogs,text="2",padx="40",pady="20",command=lambda:btnclick(2))
+btn3=Button(manslogs,text="3",padx="40",pady="20",command=lambda:btnclick(3))
+btn4=Button(manslogs,text="4",padx="40",pady="20",command=lambda:btnclick(4))
+btn5=Button(manslogs,text="5",padx="40",pady="20",command=lambda:btnclick(5))
+btn6=Button(manslogs,text="6",padx="40",pady="20",command=lambda:btnclick(6))
+btn7=Button(manslogs,text="7",padx="40",pady="20",command=lambda:btnclick(7))
+btn8=Button(manslogs,text="8",padx="40",pady="20",command=lambda:btnclick(8))
+btn9=Button(manslogs,text="9",padx="40",pady="20",command=lambda:btnclick(9))
+
+btnsum=Button(manslogs,text="+",padx="40",pady="20",command=lambda:btncommand("+"))
+btnsub=Button(manslogs,text="-",padx="40",pady="20",command=lambda:btncommand("-"))
+btnmul=Button(manslogs,text="x",padx="40",pady="20",command=lambda:btncommand("x"))
+btndiv=Button(manslogs,text=":",padx="40",pady="20",command=lambda:btncommand(":"))
+btnclear=Button(manslogs,text="C",padx="40",pady="20")
 btnequals=Button(manslogs,text="=",padx="40",pady="20")
 
 btn1.grid(row=1,column=1)
